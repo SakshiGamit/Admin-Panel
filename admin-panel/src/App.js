ae4317f2591947822bfe2a2a2908ddefce02c506
Sakshi <sakshigamit.avm@gmail.com>
2024-03-27 13:01:30 +0530

    modified

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element = {<Register />} />
         <Route path="/login" element = {<Login />} />
+        <Route path="/register" element = {<Register />} />
         <Route path="/navbar" element = {<Navbar/>} />
         <Route path="*" element = {<Error />} />
       </Routes>
